b5fc447092345606049f1b7f0106463137d8bf45
lutrapp <lutrapp@gmail.com>
2020-12-17 16:28:07 -0300

    incluídas as funcões de login e autenticação

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,10 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('agenda/', views.lista_eventos),
     path('', RedirectView.as_view(url='/agenda/')),
-#    path('', views.index),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user),
+
+    #    path('', views.index),
 
 ]
